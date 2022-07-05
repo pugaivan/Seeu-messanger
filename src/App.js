8b89da9a9760088acdf5ab3fb124a5a49dcd42f2
pugaivan <unsaightly@gmail.com>
2022-07-06 00:28:25 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from '../src/login/login'
+import Registration from '../src/registration/registration'
+
+const App = () => (
+  <div>
+    <Login />
+    <Registration />
+  </div>
+)
+
+export default App;
