1bbac6e4cb09accb05923ee8a3125c510b5b9f5d
pugaivan <unsaightly1234>
2022-07-11 10:29:13 +0300

    upload hide/show img, styled both

--- src/components/passwordInput/passwordInput.js
+++ src/components/passwordInput/passwordInput.js
@@ -1,5 +1,8 @@
 import Input from "../input/input"
 import React, { useState } from "react"
+import "./passwordInput.scss"
+
+
 
 
 
@@ -11,7 +14,7 @@ const PasswordInput = (props) => {
     }
 
     return (
-        <Input {...props} type={isPasswordShown ? "text" : "password"} ><button type="button" onClick={onShowPassword}>{isPasswordShown ? "Hide" : "Show"}</button></Input>
+        <Input {...props} type={isPasswordShown ? "text" : "password"} ><button class="images-container" type="button" onClick={onShowPassword}>{isPasswordShown ? <img src="/images/hide.png" width="30px" height="30px"/> : <img src="/images/show.jpg" width="30px" height="30px"/>}</button></Input>
     )
 }
 
