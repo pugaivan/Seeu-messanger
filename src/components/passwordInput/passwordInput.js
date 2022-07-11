0ceeb22470638c97cc3621d6359736a5d09f817a
pugaivan <unsaightly@gmail.com>
2022-07-12 00:37:40 +0300

    mistake

--- src/components/passwordInput/passwordInput.js
+++ src/components/passwordInput/passwordInput.js
@@ -2,10 +2,6 @@ import Input from "../input/input"
 import React, { useState } from "react"
 import "./passwordInput.scss"
 
-
-
-
-
 const PasswordInput = (props) => {
     const [isPasswordShown, setIsPasswordShown] = useState(false)
 
@@ -14,7 +10,7 @@ const PasswordInput = (props) => {
     }
 
     return (
-        <Input {...props} type={isPasswordShown ? "text" : "password"} ><button class="images-container" type="button" onClick={onShowPassword}>{isPasswordShown ? <img src="/images/hide.png" width="30px" height="30px"/> : <img src="/images/show.jpg" width="30px" height="30px"/>}</button></Input>
+        <Input {...props} type={isPasswordShown ? "text" : "password"} ><button className="images-container" type="button" onClick={onShowPassword}>{isPasswordShown ? <img src="/images/hide.png" width="30px" height="30px" /> : <img src="/images/show.jpg" width="30px" height="30px" />}</button></Input>
     )
 }
 
--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -4,6 +4,10 @@ import Link from "../../components/link/link"
 import React, { useState } from "react"
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { validate } from "../../utils/validation";
+import PasswordInput from "../../components/passwordInput/passwordInput"
+import { FORM_FIELDS } from "../../utils/constans"
+
+const { PHONE, PASSWORD, LASTNAME, FIRSTNAME } = FORM_FIELDS;
 
 const Registration = () => {
   const [phoneNumber, setRegistrationNumber] = useState(null);
@@ -12,42 +16,37 @@ const Registration = () => {
   const [lastName, setFastName] = useState(null);
   const [errors, setErrros] = useState({});
 
-
   const onRegistrationNumberChange = event => setRegistrationNumber(event.target.value);
   const onRegistrationPasswordChange = event => setRegistrationPassword(event.target.value);
   const onFirstNameChange = event => setFirstName(event.target.value);
   const onLastNameChange = event => setFastName(event.target.value);
 
-  console.log(firstName, lastName)
-
   const formSubmin = (event) => {
     event.preventDefault();
 
     const validationErrors = validate({
-      phoneNumber: {
+      [PHONE]: {
         required: true,
         value: phoneNumber
       },
-      password: {
+      [PASSWORD]: {
         required: true,
         value: password
       },
-      firstName: {
+      [FIRSTNAME]: {
         required: true,
         value: firstName
       },
-      lastName: {
+      [LASTNAME]: {
         required: true,
         value: lastName
       }
-
-
     }, { ...errors })
     setErrros({ ...validationErrors })
   }
 
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const navigateToContacts = () => {
     navigate('/login');
   };
@@ -57,10 +56,10 @@ const Registration = () => {
       <div className="form-container">
         <h2>Registration</h2>
         <form onSubmit={formSubmin}>
-          <Input placeholder="Phone" id="phone-number" label="Phone number" type="text" onChange={onRegistrationNumberChange} errors={errors} name="phoneNumber" />
-          <Input placeholder="Password" id="password" label="Your password" type="password" onChange={onRegistrationPasswordChange} errors={errors} name="password" />
-          <Input placeholder="First name" id="first-name" label="Your first name" type="text" onChange={onFirstNameChange} errors={errors} name="firstName" />
-          <Input placeholder="Last name" id="last-name" label="Your last name" type="text" onChange={onLastNameChange} errors={errors} name="lastName" />
+          <Input placeholder="Phone" id="phone-number" label="Phone number" type="text" onChange={onRegistrationNumberChange} errors={errors} name={PHONE} />
+          <PasswordInput placeholder="Password" id="password" label="Your password" type="password" onChange={onRegistrationPasswordChange} errors={errors} name={PASSWORD} />
+          <Input placeholder="First name" id="first-name" label="Your first name" type="text" onChange={onFirstNameChange} errors={errors} name={FIRSTNAME} />
+          <Input placeholder="Last name" id="last-name" label="Your last name" type="text" onChange={onLastNameChange} errors={errors} name={LASTNAME} />
           <Link text="Do you already have an account?" onClick={navigateToContacts} type="Log in" />
           <Button text="Register" />
           <Routes>
