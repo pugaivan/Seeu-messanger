16eedf44110db936e6df872d6968bbf33fde580c
pugaivan <unsaightly@gmail.com>
2022-07-26 17:25:57 +0300

    did divs resize

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -9,3 +9,33 @@ export const parseErrorMessage = (error) => {
         return 'Something went wrong, please try again later'
     }
 }
+
+export const resizeSides = () => {
+    let isResizing = false;
+
+    (function () {
+        const container = document.getElementById("container");
+        const left = document.getElementById("left_panel");
+        const right = document.getElementById("right_panel");
+        const handle = document.getElementById("drag");
+
+        handle.onmousedown = function (e) {
+            isResizing = true;
+        };
+
+        document.onmousemove = function (e) {
+            if (!isResizing) {
+                return;
+            }
+
+            let offsetRight = container.clientWidth - (e.clientX - container.offsetLeft);
+
+            left.style.right = offsetRight + "px";
+            right.style.width = offsetRight + "px";
+        }
+
+        document.onmouseup = function (e) {
+            isResizing = false;
+        }
+    })();
+}
