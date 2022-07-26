c708d184ee1418ef30a53f8756ab80a6ef893767
pugaivan <unsaightly@gmail.com>
2022-07-26 17:25:22 +0300

    did working state, tabs name wrote into constant TABS

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,34 @@ import Button from "../components/button/button"
 import React, { useState } from "react"
 
 import "./index.scss"
+import { resizeSides } from "../utils/helper"
+
+window.onload = function () {
+    resizeSides();
+};
+
+const TABS = {
+    messages: 'messages',
+    contacts: 'contacts'
+}
 
 const Home = () => {
 
     const [activeTab, setActiveTab] = useState('messages')
 
     return (
-        <div className="page-wrapper">
-            <div className="left-content">
+        <div className="page-wrapper" id="container">
+            <div className="left-content" id="left_panel">
                 {activeTab === 'messages' ? <div>messages</div> : <div>contacts</div>}
                 <div className="tabs-container">
-                    <button>messages</button>
-                    <button>contacts</button>
+                    <button onClick={() => setActiveTab('messages')}>{TABS.messages}</button>
+                    <button onClick={() => setActiveTab('contacts')}>{TABS.contacts}</button>
                 </div>
             </div>
-            <div className="right-content" />
+            <div className="right-content" id="right_panel" >
+                right content!
+                <div id="drag"></div>
+            </div>
 
 
         </div>
